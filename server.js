22b5f35b9fc4a14f6401141371988654dfe2dfd0
Shinta Nursobah Chairani <shinta.nur505@gmail.com>
2026-09-04 05:44:45 +0700

    Fix static file serving path in server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
-app.use(express.static(__dirname, 'public'));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors());
 
 app.get('/', (req, res) => {
